15e4bdfb0ebec8c4a89a6ddf9a1df14843e15cbb
GanzosRozas <Javi282015@gmail.com>
2023-05-05 15:00:45 -0700

    Inclusion de audio inicial
    Solo se incluye la bienvenida luego le pondré musiquita para ambientar

--- Grammar Frenzy/Form1.cs
+++ Grammar Frenzy/Form1.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin.Animations;
+using System.Media;
 
 namespace Grammar_Frenzy
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
+        SoundPlayer Audios;
         public Form1()
         {
             InitializeComponent();
         }
+        public string getPath(string cancion)
+        {
+            string[] s = { "\\bin" };
+            string path = Application.StartupPath.Split(s, StringSplitOptions.None)[0] + "\\Audios\\" + cancion;
+
+            return path;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cancion = getPath("Welcome.wav");
+
+            Audios = new SoundPlayer(cancion);
+
+
+            Audios.Load();
+            Audios.PlayLooping();
+        }
     }
+
+    
+
 }
